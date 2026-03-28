ffde1e8abab04e1c6b7d2ac8215d3860a769d455
Zaid Abbas <zaid.sarmad2006@gmail.com>
2026-03-28 00:20:49 -0700

    part 1

--- Day08.py
+++ Day08.py
@@ -1,6 +1,8 @@
-# maybe not an actual Disjoint-set data structure in the traditional sense
+from itertools import combinations
+#  maybe not an actual Disjoint-set data structure in the traditional sense
 # but it gets the job done for this specific implementation
 # will also be slower than normal union-find
+# also, this implementation won't work for part 2 I don't think...maybe change case 5?
 class unionFind:
     def __init__(self):
         # list of the different empty sets
@@ -39,5 +41,34 @@ class unionFind:
         # takes the union of sets i and j; removes j from the list
         self.sets[i] = self.sets[i] | self.sets[j]
         self.sets.pop(j)
+    def largestThreeProd(self):
+        lengths = [len(s) for s in self.sets]
+        lengths.sort(reverse=True)
+        return lengths[0] * lengths[1] * lengths[2]
+    def __str__(self):
+        return self.sets
 
-lines = [line.strip() for line in open("inputs/test.txt", "r")]
+lines = [line.strip() for line in open("inputs/Day08.txt", "r")]
+cleaned_lines =  [tuple(map(int, line.split(","))) for line in lines] # I hate python
+
+def partOne():
+    circuits = unionFind()
+    results = []
+    
+    for (i, p1), (j, p2) in combinations(enumerate(cleaned_lines), 2):
+        # order by square of distance so we don't have annoying FP issues
+        dist = (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2
+        results.append((dist, i, j, p1, p2))
+    
+    results.sort(key=lambda x: x[0])
+
+    for i in range(1000):
+        d, x, y, p1, p2 = results[i]
+        circuits.add((x, y))
+    print(circuits.largestThreeProd())
+
+def partTwo():
+    # so it needs to connect ALL of them into one circuit?
+    print('Hello, World!')
+
+partOne()
